ec779251daff0d270076dd5139a078aa3df99924
jpjuradoe <142858531+jpjuradoe@users.noreply.github.com>
2023-09-26 16:16:21 -0400

    Update tokeniser.py

--- practicals/tokeniser.py
+++ practicals/tokeniser.py
@@ -1,10 +1,25 @@
 import sys
 
-str = sys.stdin.read()
+a = 0
+c = '.\n'
+c = c.replace(' ', ' .')
 
-lista = [[" ", "\n"], [".", " ."], [",", " ,"], [":", " :"], [";", " ;"],["-", " - "],["(", " ( "], [")", " ) "]]
-
-for punctuation, space in lista:
-	str = str.replace(punctuation, space)
-
-print(str)
+for line in sys.stdin.readlines():
+        if line.strip() == '':
+                continue
+        a = a + 1
+        b = 0
+        print('\n# sent_id =', a)
+        print('# text =', line)
+        line = line.replace('.', ' . ')
+        line = line.replace(',', ' , ')
+        line = line.replace(':', ' : ')
+        line = line.replace(';', ' ; ')
+        line = line.replace('-', ' - ')
+        line = line.replace('(', ' ( ')
+        line = line.replace(')', ' ) ')
+        line = line.replace('"', ' " ')
+        tokens = line.split()
+        for token in tokens:
+                b = b + 1
+                print(b, token, '\t_\t_\t_\t_\t_\t_\t_\t_\t_\t_')
