8b6b3b263c8229e230d1f62a9a002dee447666e9
jpjuradoe <142858531+jpjuradoe@users.noreply.github.com>
2023-11-30 21:02:54 -0500

    Update segmenter.py

--- practicals/segmenter.py
+++ practicals/segmenter.py
@@ -4,8 +4,7 @@ import sys
 
 line = sys.stdin.readline()
 
-lines = line.replace('.', '.\n')
-
-while lines:
-	print(lines)
-	lines = sys.stdin.readline()
+while line:
+	line = line.replace('.', '.\n')
+	print(line)
+	line = sys.stdin.readline()
