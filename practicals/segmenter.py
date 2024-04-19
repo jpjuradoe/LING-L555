ff6b16907b4c14de2c3d1365515b9d68c9d0e5f1
jpjuradoe <142858531+jpjuradoe@users.noreply.github.com>
2024-04-19 16:46:32 -0400

    Update segmenter.py

--- practicals/segmenter.py
+++ practicals/segmenter.py
@@ -1,10 +1,3 @@
 #To replace every '. ' with a full stop and a newline character  '\n\'
 
-import sys
 
-line = sys.stdin.readline()
-
-while line:
-	line = line.replace('.', '.\n')
-	print(line)
-	line = sys.stdin.readline()
